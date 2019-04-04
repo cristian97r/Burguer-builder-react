e3a85589f5d1664e058b027c4db5bc89afb3d45e
cristian97r <cristian97drb@gmail.com>
2019-04-04 16:23:13 -0400

    add react-router  and checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,30 +87,41 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true})
+        // this.setState({loading: true})
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice.toFixed(2),
-            customer: {
-                name: 'Cristian Romero',
-                address: {
-                    street: 'testStreet 2',
-                    zipCode: '12345',
-                },
-                email: 'test@test.com'
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice.toFixed(2),
+        //     customer: {
+        //         name: 'Cristian Romero',
+        //         address: {
+        //             street: 'testStreet 2',
+        //             zipCode: '12345',
+        //         },
+        //         email: 'test@test.com'
 
-            },
-            deliveryMethod: "fastest"
+        //     },
+        //     deliveryMethod: "fastest"
+        // }
+
+        // // axios.post('orders.json', order)
+        // // .then(response => {
+        // //     this.setState({loading: false, purchasing: false})
+        // // })
+        // // .catch(error => {
+        // //     this.setState({loading: false, purchasing: false})
+        // // })
+        const queryParams = []
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        const queryString = queryParams.join('&')
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
         
-        axios.post('orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false})
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing: false})
-        })
     }
 
     render () {
